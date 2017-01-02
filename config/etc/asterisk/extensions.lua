23f5a74def5f7e7ed4a9537068d23d74589bcea2
Sergey Dmitriev <serge.dmitriev@gmail.com>
2017-01-02 21:04:40 +0700

    fix

--- config/etc/asterisk/extensions.lua
+++ config/etc/asterisk/extensions.lua
@@ -36,15 +36,16 @@ local httprequest = function (context, extension)
     
     local t1 = os.clock();
     
-    local url = "http://localhost:3000/loko";
+    local url = "http://192.168.1.38:3000/loko";
 
     local headers, stream = assert(http_request.new_from_uri(url):go())
     local body = assert(stream:get_body_as_string())
-    if headers:get ":status" ~= "200" then
+    if headers:get(":status") ~= "200" then
         error(body)
-    end
-    local lua_value = JSON:decode(body)
-    app.noop(inspect(lua_value))
+    end;
+
+    local lua_value = JSON:decode(body);
+    app.noop(inspect(lua_value));
 
     app.noop('difftime: '..tostring(os.clock() - t1));
 end
