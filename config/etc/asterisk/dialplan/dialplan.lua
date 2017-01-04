edebef2de88e0a6f4bcc49fab3881930aea8fe7d
Sergey Dmitriev <serge.dmitriev@gmail.com>
2017-01-04 17:38:54 +0700

    fix

---
+++ config/etc/asterisk/dialplan/dialplan.lua
@@ -0,0 +1,60 @@
+
+--local request = require('/etc/asterisk/dialplan/1');
+
+local dial = function (context, extension)
+    app.noop("context: " .. context .. ", extension: " .. extension);
+    app.dial('SIP/' .. extension, 10);
+    
+    local dialstatus = channel["DIALSTATUS"]:get();
+    app.noop('dialstatus: '..dialstatus);
+    app.set("CHANNEL(language)=ru");
+
+    if dialstatus == 'BUSY' then
+        app.playback("followme/sorry");        
+    elseif dialstatus == 'CHANUNAVAIL' then 
+        app.playback("followme/sorry");
+    end;
+
+    app.hangup();
+end;
+
+local ivr = function (context, extension)        
+    app.read("IVR_CHOOSE", "/var/menu/demo", 1, nil, 2, 3);
+    local choose = channel["IVR_CHOOSE"]:get();
+
+    if choose == '1' then
+        app.queue('1234');
+    elseif choose == '2' then
+        dial('internal', '101');
+    else
+        app.hangup();
+    end;
+end;
+
+
+
+local Dialplan = {
+    getExtensions = function ()
+        return {
+            ["internal"] = {
+                ["*12"] = function ()
+                    app.sayunixtime();
+                end;
+
+                ["_100"] = request;
+
+                ["_1XX"] = dial;
+
+                ["200"] = ivr;
+            };           
+        };
+    end;
+
+    getHints = function ()
+        return {
+
+        };
+    end;
+};
+
+return Dialplan;
