7d09d49c26154c6bca3f530e8cd22f6e4d313c54
Sergey Dmitriev <serge.dmitriev@gmail.com>
2017-01-15 21:40:46 +0700

    fix dialplan

--- config/etc/asterisk/dialplan/1.lua
+++ config/etc/asterisk/dialplan/1.lua
@@ -1,31 +1,24 @@
 local inspect = require "inspect";
---local http_request = require "http.request"
+local httpclient = require "httpclient"
 local JSON = require "JSON"
 
 local httprequest = function (context, extension)
-    
+
     local t1 = os.clock();
-    
-    local url = "http://192.168.1.41:3000/loko";
 
-    hc = require('httpclient').new()
-    res = hc:get(url)
+    local url = "http://192.168.1.41:3000/trunk";
+
+    local hc = httpclient.new();
+    local res = hc:get(url);
+
     if res.body then
-      print(res.body)
+      print(res.body);
     else
-      print(res.err)
-    end
-
-    --[[
-    local headers, stream = assert(http_request.new_from_uri(url):go())
-    local body = assert(stream:get_body_as_string())
-    if headers:get(":status") ~= "200" then
-        error(body)
+      print(res.err);
     end;
-    ]]
+
     local lua_value = JSON:decode(res.body);
     app.noop(inspect(lua_value));
-    
 
     app.noop('difftime: '..tostring(os.clock() - t1));
 end
