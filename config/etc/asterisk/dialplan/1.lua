df614eb62ef3cfe01b18fa265dd50d475196d20e
Sergey Dmitriev <serge.dmitriev@gmail.com>
2017-03-09 22:58:39 +0700

    use token some times

--- config/etc/asterisk/dialplan/1.lua
+++ config/etc/asterisk/dialplan/1.lua
@@ -1,55 +1,87 @@
 local inspect = require "inspect";
 local httpclient = require "httpclient"
 local JSON = require "JSON"
+local redis = require 'redis'
+
+local rclient = redis.connect('127.0.0.1', 6379)
+
+local resurl = 'http://192.168.1.41:3030/';
+
+local jwtrequest = function (tokenIn)
+
+    local hc = httpclient.new();
+    local authenticated = false;
+    local token = tokenIn;
+
+    local authByLoginPassword = function ()
+        local authurl = resurl .. 'auth/local';
+
+        local postdata = {
+            ['type'] = "local";
+            ['password'] = "pulivu";
+            ['phone'] = "9135292926";
+        };
+
+        local data = JSON:encode(postdata);
+        app.noop("json:"..data);
+
+        local res = hc:post(authurl, data, {content_type = "application/json"});
+        app.noop("post: "..inspect(res));
+
+        local token = JSON:decode(res.body).token;
+        
+        return token;
+    end
+
+    local makeRequest = function ()
+        local url = resurl .. 'payments';
+        app.noop('url: '..url);
+
+        local headers = {
+            authorization = token;
+        };
+
+        local res = hc:get(url, {headers = headers});
+
+
+        return res;
+    end;
+    
+    local res = makeRequest()
+
+    app.noop('rees:'..inspect(res));
+    app.noop('code:'..inspect(res.code))
+
+    if res.code == 401 then 
+       token = authByLoginPassword()
+       res = makeRequest()
+    end;
+    
+    return res, token;
+    
+end;
+
 
 local httprequest = function (context, extension)
     
-    local hc = httpclient.new();
+    
     local t1 = os.clock();
 
-    local authurl = 'http://192.168.1.41:3030/auth/local';
-    
-    local headers = {
-        ['content-type'] = "application/json";
-    };
+    local t = rclient:get('token');
 
-    local postdata = {
-        ['type'] = "local";
-        ['password'] = "pulivu";
-        ['phone'] = "9135292926";
-    }
+--    local t = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiaWF0IjoxNDg4OTUxODQwLCJleHAiOjE0ODkwMzgyNDAsImlzcyI6ImZlYXRoZXJzIn0.1Ysfrc-JCN9o1vyltxZbnGFLO4AL62zfwofU5oD5QU0";
+    local res, token = jwtrequest(t);
 
-    local d = JSON:encode(postdata);
-    app.noop("json:"..d);
-    local res = hc:post(authurl, d, {content_type = "application/json"});
+    rclient:set('token', token);
 
-
-    app.noop("post: "..inspect(res));
-
-
-    local token = JSON:decode(res.body).token;
-
-    -- local url = "http://192.168.1.41:3000/trunk";
-    local url = 'http://192.168.1.41:3030/payments';
-    -- local token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiaWF0IjoxNDg4OTUxODQwLCJleHAiOjE0ODkwMzgyNDAsImlzcyI6ImZlYXRoZXJzIn0.1Ysfrc-JCN9o1vyltxZbnGFLO4AL62zfwofU5oD5QU0";
-
-
-
-
-    app.noop('url: '..url);
-    local headers = {
-        authorization = token;
-    };
-
-    
-    local res = hc:get(url, {headers = headers});
-
+    app.noop('response:'..inspect(res));
 
     if res.body then
       app.noop(res.body);
     else
       app.noop(res.err);
     end;
+
 
     local lua_value = JSON:decode(res.body);
     app.noop(inspect(lua_value));
